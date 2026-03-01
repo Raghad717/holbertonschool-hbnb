2f1f43826c41d3f1632128d88cdd540c0ea753fd
jana <bkeyh382@gmail.com>
2026-03-01 11:57:52 +0300

    Update place.py

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -1,1 +1,31 @@
+from app.extensions import db
+from app.models.base_model import BaseModel
 
+class Place(BaseModel):
+    __tablename__ = 'places'
+    
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    owner_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
+    
+    # Relationships
+    reviews = db.relationship('Review', backref='place', lazy=True, cascade='all, delete-orphan')
+    amenities = db.relationship('Amenity', secondary='place_amenity', lazy='subquery',
+                               backref=db.backref('places', lazy=True))
+    
+    def to_dict(self):
+        base_dict = super().to_dict()
+        base_dict.update({
+            'title': self.title,
+            'description': self.description,
+            'price': self.price,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'owner_id': self.owner_id,
+            'reviews': [review.to_dict() for review in self.reviews],
+            'amenities': [amenity.to_dict() for amenity in self.amenities]
+        })
+        return base_dict
