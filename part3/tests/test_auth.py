eb61cfc886628ebd47e37842b81ac52f4b52894c
csrama <156553209+csrama@users.noreply.github.com>
2026-03-01 12:32:44 +0300

    Update test_auth.py

--- part3/tests/test_auth.py
+++ part3/tests/test_auth.py
@@ -5,13 +5,11 @@ from app.models.user import User
 
 class TestAuth(unittest.TestCase):
     def setUp(self):
-        
         self.app = create_app('testing')
         self.client = self.app.test_client()
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
-        
         
         user = User(
             first_name='Test',
@@ -24,14 +22,13 @@ class TestAuth(unittest.TestCase):
         db.session.commit()
     
     def tearDown(self):
-
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
     
     def test_login_success(self):
-     
-        response = self.client.post('/api/v1/auth/login',
+        response = self.client.post(
+            '/api/v1/auth/login',
             json={'email': 'test@test.com', 'password': 'password123'}
         )
         self.assertEqual(response.status_code, 200)
@@ -39,15 +36,15 @@ class TestAuth(unittest.TestCase):
         self.assertIn('access_token', data)
     
     def test_login_wrong_password(self):
-       
-        response = self.client.post('/api/v1/auth/login',
+        response = self.client.post(
+            '/api/v1/auth/login',
             json={'email': 'test@test.com', 'password': 'wrong'}
         )
         self.assertEqual(response.status_code, 401)
     
     def test_login_user_not_found(self):
-
-      response = self.client.post('/api/v1/auth/login',
+        response = self.client.post(
+            '/api/v1/auth/login',
             json={'email': 'notfound@test.com', 'password': 'password123'}
         )
         self.assertEqual(response.status_code, 401)
