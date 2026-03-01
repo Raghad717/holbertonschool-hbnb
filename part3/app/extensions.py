fa623c5275f117a265a9054238ad9ec4efc79acc
jana <bkeyh382@gmail.com>
2026-03-02 00:06:03 +0300

    Update extensions.py

--- part3/app/extensions.py
+++ part3/app/extensions.py
@@ -1,8 +1,13 @@
-"""Extensions initialization."""
+"""Initialize Flask extensions."""
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 
+# Database ORM
 db = SQLAlchemy()
+
+# JWT authentication
 jwt = JWTManager()
+
+# Password hashing
 bcrypt = Bcrypt()
