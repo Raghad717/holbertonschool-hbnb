27df52657b40f418c14e1a22fd7fe7fd3521fc30
csrama <156553209+csrama@users.noreply.github.com>
2026-03-08 11:34:52 +0300

    Update users.py

--- part3/app/api/v1/users.py
+++ part3/app/api/v1/users.py
@@ -1,23 +1,30 @@
 from flask_restx import Namespace, Resource, fields
 from flask import request
-from app.models.user import User
 from app.services.facade import HBnBFacade
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-api = Namespace('users', description='User operations')
+# Namespace (this must match what __init__.py imports)
+users_bp = Namespace('users', description='User operations')
+
 facade = HBnBFacade()
 
-user_model = api.model('User', {
+# Model for API documentation
+user_model = users_bp.model('User', {
     'first_name': fields.String(required=True),
     'last_name': fields.String(required=True),
     'email': fields.String(required=True),
     'password': fields.String(required=True)
 })
 
-@api.route('/')
+
+@users_bp.route('/')
 class UserList(Resource):
-    @api.expect(user_model)
+
+    @users_bp.expect(user_model)
     def post(self):
+        """Create a new user"""
         data = request.get_json()
-        facade.create_user(data)
-        return user.to_dict(), 201
+
+        response, status = facade.create_user(data)
+
+        return response, status
