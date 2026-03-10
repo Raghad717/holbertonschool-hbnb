117bcb6f33a6ad86b323c79905046e49cb26eec3
Raghad717 <r4gh4dalmalki@gmail.com>
2026-03-10 04:33:20 +0300

    Update __init__.py

--- part3/app/api/__init__.py
+++ part3/app/api/__init__.py
@@ -1,1 +1,16 @@
+# app/api/v1/__init__.py
+from flask_restx import Api
+from flask import Blueprint
+from app.api.v1.users import api as users_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.amenities import api as amenities_ns
 
+bp_v1 = Blueprint("api_v1", __name__)
+api = Api(bp_v1, title="HBnB API", version="1.0", description="HBnB API v1")
+
+# Register namespaces
+api.add_namespace(users_ns, path="/users")
+api.add_namespace(places_ns, path="/places")
+api.add_namespace(reviews_ns, path="/reviews")
+api.add_namespace(amenities_ns, path="/amenities")
