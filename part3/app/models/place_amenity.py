3de1760df247485ba0c0d43f2a960b5991c09038
Raghad717 <r4gh4dalmalki@gmail.com>
2026-03-12 21:36:31 +0300

    Update place_amenity.py

--- part3/app/models/place_amenity.py
+++ part3/app/models/place_amenity.py
@@ -3,5 +3,6 @@ from app.extensions import db
 place_amenity = db.Table(
     "place_amenity",
     db.Column("place_id", db.String(36), db.ForeignKey("places.id"), primary_key=True),
-    db.Column("amenity_id", db.String(36), db.ForeignKey("amenities.id"), primary_key=True)
+    db.Column("amenity_id", db.String(36), db.ForeignKey("amenities.id"), primary_key=True),
+    extend_existing=True
 )
