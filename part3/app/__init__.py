ce515b15d3f25b639b62efa934181e716ac18380
jana <bkeyh382@gmail.com>
2026-04-11 23:49:46 +0300

    Refactor create_app function and formatting

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,27 +1,19 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS 
 from app.extensions import db, jwt, bcrypt
-from app.api.v1 import bp_v1  
+from app.api.v1 import bp_v1
 from config import config_dict
 
-
 def create_app(config_name="development"):
-    """
-    Factory function to create Flask app with:
-    - Extensions initialized
-    - Blueprint registered
-    - Config loaded from config_dict
-    """
     app = Flask(__name__)
     app.config.from_object(config_dict[config_name])
-    CORS(app)
-
-    # Initialize extensions
+    
+    CORS(app)  
+    
     db.init_app(app)
     jwt.init_app(app)
     bcrypt.init_app(app)
-
-    # Register the main API blueprint (v1)
+    
     app.register_blueprint(bp_v1, url_prefix="/api/v1")
-
+    
     return app
